b61c09f413b700f02405a7665deb32c05aa44fa7
Yasin Bilgin <stdxpe@gmail.com>
2024-07-11 21:40:29 +0300

    color hex-codes of light mode theme added

---
+++ lib/utilities/k_color_palette.dart
@@ -0,0 +1,51 @@
+import 'dart:ui';
+
+class ColorPalette {
+  /// [APP WIDE GLOBAL VARIABLE]
+  /// COLOR HEX-CODES OF LIGHT/DARK MODE THEMES
+
+  /// [LIGHT THEME MODE]
+  static const Color scaffoldBackground = Color(0xFFFFFFFF);
+  static const Color buttonMainBackgroundPrimary = Color(0xFF232429);
+  static const Color buttonMainForegroundPrimary = Color(0xFFFFFFFF);
+  static const Color buttonMainBackgroundSecondary = Color(0xFFFFFFFF);
+  static const Color buttonMainForegroundSecondary = Color(0xFF000000);
+  static const Color title = Color(0xFF000000);
+  static const Color text = Color(0xFF000000);
+  static const Color fadedText = Color(0xFF9D9D9D);
+  static const Color fadedTextButton = Color(0xFF393939);
+  static const Color cardBackground = Color(0xFFFFFFFF);
+  static const Color cardTitle = Color(0xFF000000);
+  static const Color cardSubtitle = Color(0xFF737373);
+  static const Color cardPrice = Color(0xFF000000);
+  static const Color bottomSheetBackground = Color(0xFFF9F9F9);
+  static const Color paymentStatusActive = Color(0xFF1D1D1D);
+  static const Color paymentStatusPassive = Color(0xFFE3E3E3);
+  static const Color textfield = Color(0xFFFFFFFF);
+  static const Color navigationBarBackground = Color(0xFF232429);
+  static const Color navigationBarActive = Color(0xFF0D0D0D);
+  static const Color navigationBarPassive = Color(0xFF898989);
+
+  /// [DARK THEME MODE]
+  /// TODO: Dark Mode Color Selection
+  static const Color darkModeScaffoldBackground = Color(0xFFFFFFFF);
+  static const Color darkModeButtonMainBackgroundPrimary = Color(0xFF232429);
+  static const Color darkModeButtonMainForegroundPrimary = Color(0xFFFFFFFF);
+  static const Color darkModeButtonMainBackgroundSecondary = Color(0xFFFFFFFF);
+  static const Color darkModeButtonMainForegroundSecondary = Color(0xFF000000);
+  static const Color darkModeTitle = Color(0xFF000000);
+  static const Color darkModeText = Color(0xFF000000);
+  static const Color darkModeFadedText = Color(0xFF9D9D9D);
+  static const Color darkModeFadedTextButton = Color(0xFF393939);
+  static const Color darkModeCardBackground = Color(0xFFFFFFFF);
+  static const Color darkModeCardTitle = Color(0xFF000000);
+  static const Color darkModeCardSubtitle = Color(0xFF737373);
+  static const Color darkModeCardPrice = Color(0xFF000000);
+  static const Color darkModeBottomSheetBackground = Color(0xFFF9F9F9);
+  static const Color darkModePaymentStatusActive = Color(0xFF1D1D1D);
+  static const Color darkModePaymentStatusPassive = Color(0xFFE3E3E3);
+  static const Color darkModeTextfield = Color(0xFFFFFFFF);
+  static const Color darkModeNavigationBarBackground = Color(0xFF232429);
+  static const Color darkModeNavigationBarActive = Color(0xFF0D0D0D);
+  static const Color darkModeNavigationBarPassive = Color(0xFF898989);
+}

477baabdc898eb5cce51fb83481182a2ec9140e7
Yasin Bilgin <stdxpe@gmail.com>
2024-07-11 19:26:17 +0300

    vcs test

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ECommerce Shopping Project',
       theme: ThemeData(
         // This is the theme of your application.
         //

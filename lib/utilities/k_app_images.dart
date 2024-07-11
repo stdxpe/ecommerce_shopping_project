4837959a2d99dc3669edba7156e273bc55a17736
Yasin Bilgin <stdxpe@gmail.com>
2024-07-11 19:48:31 +0300

    app wide local image paths added

---
+++ lib/utilities/k_app_images.dart
@@ -0,0 +1,20 @@
+class AppImages {
+  /// [APP WIDE GLOBAL VARIABLE]
+  /// LOCAL PATHS OF IMAGES
+
+  static const String productImage0 = "assets/images/pose0.jpg";
+  static const String productImage1 = "assets/images/pose1.jpg";
+  static const String productImage2 = "assets/images/pose2.jpg";
+  static const String productImage3 = "assets/images/pose3.jpg";
+  static const String productImage4 = "assets/images/pose4.jpg";
+  static const String productImage5 = "assets/images/pose5.jpg";
+  static const String productImage6 = "assets/images/pose6.jpg";
+  static const String productImage7 = "assets/images/pose7.jpg";
+  static const String productImage8 = "assets/images/pose8.jpg";
+  static const String productImage9 = "assets/images/pose9.jpg";
+  static const String productImage10 = "assets/images/pose10.jpg";
+  static const String productImage11 = "assets/images/pose11.jpg";
+  static const String productImage12 = "assets/images/pose12.jpg";
+
+  static const String creditCardBackground = "assets/images/world_map.jpg";
+}

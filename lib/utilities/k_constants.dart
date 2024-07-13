34f4ede5cf862a3d6be22a079055ab14dd56ec10
Yasin Bilgin <stdxpe@gmail.com>
2024-07-13 19:15:56 +0300

    global padding and border radius constants added

--- lib/utilities/k_constants.dart
+++ lib/utilities/k_constants.dart
@@ -4,11 +4,32 @@ abstract class Constants {
   /// [APP WIDE GLOBAL VARIABLE]
   /// LAYOUT, ALIGNMENT, PADDING AND BORDER RADIUS CONSTANTS
 
-  /// TODO: Screen Utils to-be-added
-  static const double kPaddingAppTop = 35;
-  static const double kPaddingAppBottom = 100;
-  static const double kPaddingHorizontalMain = 15;
-  static const double kPaddingButtonHorizontalMain = 30;
-  static const double kPaddingButtonBottomMain = 40;
-  static const double kPaddingButtonTopMain = 22.5;
+  /// GLOBAL PADDINGS
+  static const double kPaddingTitleTop = 77;
+  static const double kPaddingTitleBottom = 60;
+  static const double kPaddingTitleAndSubtitleBetweenVertical = 25;
+
+  static const double kPaddingCardContentTop = 60;
+  static const double kPaddingCardContentLeft = 50;
+  static const double kPaddingCardContentsBetween = 21;
+
+  static const double kPaddingMainHorizontal = 15;
+  static const double kPaddingMainBetweenCardsHorizontal = 30;
+  static const double kPaddingMainBetweenCardsVertical = 30;
+
+  static const double kPaddingButtonHorizontalMain = 90;
+  static const double kPaddingButtonBottomMain = 50;
+  static const double kPaddingButtonTopMain = 60;
+
+  static const double kPaddingDetailsScreenTitleTop = 100;
+  static const double kPaddingDetailsScreenBetweenElementsVertical = 65;
+  static const double kPaddingDetailsScreenHorizontalMain = 30;
+
+  /// GLOBAL BORDER RADIUSSES
+  static const double kRadiusButtonMain = 100;
+  static const double kRadiusDialogPopups = 50;
+  static const double kRadiusSliderCards = 30;
+  static const double kRadiusCreditCards = 35;
+  static const double kRadiusCardPrimary = 25;
+  static const double kRadiusCardSecondary = 20;
 }

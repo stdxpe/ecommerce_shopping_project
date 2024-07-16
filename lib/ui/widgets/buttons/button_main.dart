2c72a1e894350882e23cad79cbd1ebd3e15584f5
Yasin Bilgin <stdxpe@gmail.com>
2024-07-16 14:20:48 +0300

    reusable main button widget created

---
+++ lib/ui/widgets/buttons/button_main.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:ecommerce_shopping_project/utilities/utilities_library_imports.dart';
+
+class ButtonMain extends StatelessWidget {
+  const ButtonMain({
+    required this.onPressed,
+    required this.text,
+    this.backgroundColor,
+    this.foregroundColor,
+    this.borderColor,
+    this.height,
+    this.paddingHorizontal,
+    this.paddingVertical,
+    this.paddingLeft,
+    this.paddingRight,
+    this.icon,
+    this.fontSize,
+    this.fontWeight,
+    this.fontLetterSpacing,
+    this.borderWidth,
+    this.useShadow = true,
+    super.key,
+  });
+
+  final Function onPressed;
+  final String text;
+  final Color? backgroundColor;
+  final Color? foregroundColor;
+  final Color? borderColor;
+  final double? height;
+  final double? paddingHorizontal;
+  final double? paddingVertical;
+  final double? paddingLeft;
+  final double? paddingRight;
+  final Widget? icon;
+  final double? fontSize;
+  final FontWeight? fontWeight;
+  final double? fontLetterSpacing;
+  final double? borderWidth;
+  final bool? useShadow;
+
+  @override
+  Widget build(BuildContext context) {
+    double localPaddingHorizontal =
+        paddingHorizontal ?? Constants.kPaddingButtonHorizontalMain.w;
+
+    return Container(
+      height: height ?? 150.h,
+      margin: EdgeInsets.fromLTRB(
+        paddingLeft ?? localPaddingHorizontal,
+        paddingVertical ?? 0,
+        paddingRight ?? localPaddingHorizontal,
+        paddingVertical ?? 0,
+      ),
+      decoration: BoxDecoration(
+        color: context.theme.colorPalette.buttonMainBackgroundPrimary,
+        borderRadius: BorderRadius.all(
+          Radius.circular(100.r),
+        ),
+        boxShadow: [
+          if (useShadow!)
+            BoxShadows.kBoxShadowPrimary(
+              color: context.theme.colorPalette.shadowSecondary,
+            ),
+        ],
+      ),
+      child: ElevatedButton(
+        onPressed: () {
+          onPressed();
+        },
+        style: ElevatedButton.styleFrom(
+          elevation: 1,
+          side: BorderSide(
+            color: (borderWidth != null && borderWidth != 0)
+                ? borderColor ?? Colors.black87
+                : Colors.transparent,
+            width: borderWidth ?? 0,
+          ),
+          foregroundColor: foregroundColor ??
+              context.theme.colorPalette.buttonMainForegroundPrimary,
+          backgroundColor: backgroundColor ??
+              context.theme.colorPalette.buttonMainBackgroundPrimary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(100.r),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(0.0),
+          child: FittedBox(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                if (icon != null) icon!,
+                if (icon != null) SizedBox(width: 25.h),
+                FittedBox(
+                  child: Text(
+                    text,
+                    style: context.theme.textTheme.labelLarge!.copyWith(
+                      color: foregroundColor ??
+                          context
+                              .theme.colorPalette.buttonMainForegroundPrimary,
+                      fontSize: fontSize ?? 45.sp,
+                      fontWeight: fontWeight ??
+                          context.theme.textTheme.labelLarge!.fontWeight,
+                      letterSpacing: fontLetterSpacing ??
+                          context.theme.textTheme.labelLarge!.letterSpacing!.w,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

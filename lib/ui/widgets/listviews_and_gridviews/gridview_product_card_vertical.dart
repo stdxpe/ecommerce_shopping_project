7ad4c9adce5f764578dd526cd7394e03f72deaf0
Yasin Bilgin <stdxpe@gmail.com>
2024-07-18 23:56:06 +0300

    gridview product card created

---
+++ lib/ui/widgets/listviews_and_gridviews/gridview_product_card_vertical.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:ecommerce_shopping_project/models/dummy_product_list.dart';
+import 'package:ecommerce_shopping_project/models/product.dart';
+import 'package:ecommerce_shopping_project/ui/widgets/cards/product_card_vertical.dart';
+import 'package:ecommerce_shopping_project/utilities/utilities_library_imports.dart';
+
+class GridviewProductCardVertical extends StatelessWidget {
+  const GridviewProductCardVertical({
+    super.key,
+    this.itemCountOnRow = 2,
+    required this.productsList,
+    this.isCardElevated = true,
+    required this.paddingMain,
+    required this.paddingBetweenElements,
+  });
+
+  final List<Product> productsList;
+  final int? itemCountOnRow;
+  final bool? isCardElevated;
+  final double paddingMain;
+  final double paddingBetweenElements;
+
+  @override
+  Widget build(BuildContext context) {
+    double cardWidth = Constants.kRawFigmaDesignWidth;
+    int itemCount = productsList.length - ((productsList.length) % 3);
+
+    double fontSizePrimary = Constants.kPaddingCardFontHeightPrimary;
+    double fontSizeSecondary = Constants.kPaddingCardFontHeightSecondary;
+    double paddingTextVertical = Constants.kPaddingVerticalCardTopAndBottom;
+    double paddingTextBetween = Constants.kPaddingVerticalCardTextsBetween;
+
+    double textSectionHeight = paddingTextVertical +
+        paddingTextVertical +
+        paddingTextBetween +
+        fontSizePrimary +
+        fontSizeSecondary;
+
+    double totalPadding =
+        (2 * paddingMain) + ((itemCountOnRow! - 1) * paddingBetweenElements);
+
+    double cardTotalWidth = (cardWidth - totalPadding) / itemCountOnRow!;
+
+    double cardTotalHeight = cardTotalWidth.w + textSectionHeight.h;
+
+    return SizedBox(
+      width: 1.sw,
+      child: GridView.builder(
+          padding: EdgeInsets.symmetric(horizontal: paddingMain.w),
+          shrinkWrap: true,
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            childAspectRatio: cardTotalHeight / cardTotalWidth,
+            mainAxisExtent: cardTotalHeight,
+            maxCrossAxisExtent: cardTotalWidth.w,
+            mainAxisSpacing: paddingBetweenElements.w,
+            crossAxisSpacing: paddingBetweenElements.w,
+          ),
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: itemCount,
+          itemBuilder: (context, index) {
+            return ProductCardVertical(
+              product: dummyProductList[index],
+              cardWidth: cardTotalWidth,
+              isCardElevated: isCardElevated,
+              fontSizePrimary: fontSizePrimary,
+              fontSizeSecondary: fontSizeSecondary,
+              paddingTextVertical: paddingTextVertical,
+              paddingTextBetween: paddingTextBetween,
+            );
+          }),
+    );
+  }
+}

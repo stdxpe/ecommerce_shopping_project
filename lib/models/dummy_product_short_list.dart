ee31466f783865652344583d76d5b7758bbfdff2
Yasin Bilgin <stdxpe@gmail.com>
2024-07-18 23:55:44 +0300

    dummy product lists created

---
+++ lib/models/dummy_product_short_list.dart
@@ -0,0 +1,25 @@
+import 'package:ecommerce_shopping_project/models/product.dart';
+import 'package:ecommerce_shopping_project/utilities/utilities_library_imports.dart';
+
+var dummyProductShortList = [
+  Product(
+    title: 'PCR Sale-X',
+    price: 69.992342345231115111452345234535234523345234,
+    mainPhoto: AppImages.productImage10,
+  ),
+  Product(
+    title: 'Cropped Stock-Fit',
+    price: 69.992342345231115111452345234535234523345234,
+    mainPhoto: AppImages.productImage11,
+  ),
+  Product(
+    title: 'Nike AirMax XXXL-CRX',
+    price: 69.992342345231115111452345234535234523345234,
+    mainPhoto: AppImages.productImage12,
+  ),
+  Product(
+    title: 'Gucci Pantcaon',
+    price: 69.992342345231115111452345234535234523345234,
+    mainPhoto: AppImages.productImage8,
+  ),
+];

5c8762143233e4c5e4fb077c323154bba4643d02
Yasin Bilgin <stdxpe@gmail.com>
2024-07-21 13:26:42 +0300

    titles edited for responsive ui

--- lib/ui/widgets/titles/title_main.dart
+++ lib/ui/widgets/titles/title_main.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_shopping_project/ui/widgets/text_custom.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -29,47 +30,41 @@ class TitleMain extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(
-        top: paddingTop ?? Constants.kPaddingScreensTitleTop.h,
-        bottom: paddingBottom ?? Constants.kPaddingTitleBottom.h,
-        left: paddingHorizontal ?? Constants.kPaddingMainHorizontal.w,
-        right: paddingHorizontal ?? Constants.kPaddingMainHorizontal.w,
+        top: paddingTop ?? Constants.kMainTitlePaddingTop.h,
+        bottom: paddingBottom ?? Constants.kMainTitlePaddingBottom.h,
+        left: paddingHorizontal ?? Constants.kMainPaddingHorizontal.w,
+        right: paddingHorizontal ?? Constants.kMainPaddingHorizontal.w,
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           if (stepNumber != null)
-            Text(
-              '${AppStrings.paymentStep}$stepNumber',
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              style: context.theme.textTheme.titleSmall!.copyWith(
-                color: context.theme.colorPalette.titleFaded,
-                height: 1,
-              ),
+            TextCustom(
+              text: '${AppStrings.paymentStep}$stepNumber',
+              textStyle: context.textTheme.titleSmall!,
+              color: context.colorPalette.titleFaded,
             ),
-          if (stepNumber != null) SizedBox(height: 0.h),
-          // Constants.kPaddingTitleAndItemsFoundAndStepsBetweenVertical.w,
-
+          if (stepNumber != null)
+            SizedBox(
+                height:
+                    Constants.kMainTitleSpacingBTWItemsFoundBTWStepsVertical.h),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Flexible(
-                flex: 5,
-                child: Text(
-                  title,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: context.theme.textTheme.titleLarge!.copyWith(
-                    color: context.theme.colorPalette.title,
-                  ),
+                flex: 10,
+                child: TextCustom(
+                  text: title,
+                  textStyle: context.textTheme.titleLarge!,
+                  color: context.colorPalette.title,
                 ),
               ),
-              if (icon != null) Flexible(flex: 1, child: SizedBox(width: 10.w)),
+              if (icon != null) Flexible(flex: 1, child: SizedBox(width: 0.w)),
               if (icon != null)
                 Flexible(
-                  flex: 2,
+                  flex: 1,
                   child: GestureDetector(
                     onTap: () {
                       if (onPressed != null) onPressed!();
@@ -77,22 +72,21 @@ class TitleMain extends StatelessWidget {
                     child: Icon(
                       icon,
                       size: 70.w,
-                      color: context.theme.colorPalette.title,
+                      color: context.colorPalette.title,
                     ),
                   ),
                 ),
             ],
           ),
-          if (itemCount != null) SizedBox(height: 0.h),
           if (itemCount != null)
-            Text(
-              '$itemCount${AppStrings.itemsFound}',
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              style: context.theme.textTheme.titleSmall!.copyWith(
-                color: context.theme.colorPalette.titleFaded,
-                height: 0.8,
-              ),
+            SizedBox(
+                height:
+                    Constants.kMainTitleSpacingBTWItemsFoundBTWStepsVertical.h),
+          if (itemCount != null)
+            TextCustom(
+              text: '$itemCount${AppStrings.itemsFound}',
+              textStyle: context.textTheme.titleSmall!,
+              color: context.colorPalette.titleFaded,
             ),
         ],
       ),
--- lib/ui/widgets/titles/title_with_text_button.dart
+++ lib/ui/widgets/titles/title_with_text_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'package:ecommerce_shopping_project/ui/widgets/text_custom.dart';
 import 'package:ecommerce_shopping_project/utilities/utilities_library_imports.dart';
 
 class TitleWithTextButton extends StatelessWidget {
@@ -23,50 +24,36 @@ class TitleWithTextButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // color: Colors.red.withOpacity(0.5),
+    return Padding(
       padding: EdgeInsets.only(
-        top: paddingTop ?? Constants.kPaddingTitleTop.h,
-        bottom: paddingBottom ?? Constants.kPaddingTitleBottom.h,
-        left: paddingHorizontal ?? Constants.kPaddingMainHorizontal.w,
-        right: paddingHorizontal ?? Constants.kPaddingMainHorizontal.w,
+        top: paddingTop ?? Constants.kMainTitlePaddingTopForHomeScreen.h,
+        bottom: paddingBottom ?? Constants.kMainTitlePaddingBottom.h,
+        left: paddingHorizontal ?? Constants.kMainPaddingHorizontal.w,
+        right: paddingHorizontal ?? Constants.kMainPaddingHorizontal.w,
       ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           Flexible(
-            flex: 7,
-            child: Text(
-              title,
-              maxLines: 1,
-              textAlign: TextAlign.start,
-              overflow: TextOverflow.ellipsis,
-              style: context.theme.textTheme.titleLarge!.copyWith(
-                color: context.theme.colorPalette.title,
-                letterSpacing: -0.5,
-                height: 0.8,
-              ),
+            flex: 9,
+            child: TextCustom(
+              text: title,
+              textStyle: context.textTheme.titleLarge!,
+              color: context.colorPalette.title,
             ),
           ),
-          Flexible(flex: 1, child: SizedBox(width: 10.w)),
           Flexible(
             flex: 2,
             child: GestureDetector(
               onTap: () {
                 onPressed();
               },
-              child: Text(
-                buttonText,
-                maxLines: 1,
-                textAlign: TextAlign.end,
-                style: context.theme.textTheme.titleMedium!.copyWith(
-                  color: context.theme.colorPalette.textButtonFaded,
-                  fontSize: 40.sp,
-                  fontWeight: FontWeight.w700,
-                  letterSpacing: -0.5,
-                  height: 0.8,
-                ),
+              child: TextCustom(
+                text: buttonText,
+                textStyle: context.textTheme.titleMedium!,
+                color: context.colorPalette.textButtonFaded,
+                textAlignCustom: TextAlign.end,
               ),
             ),
           ),

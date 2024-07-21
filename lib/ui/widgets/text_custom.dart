513d377cf70067cb03f723868243c953eeb6fe97
Yasin Bilgin <stdxpe@gmail.com>
2024-07-21 13:26:20 +0300

    custom responsive text widget created

---
+++ lib/ui/widgets/text_custom.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TextCustom extends StatelessWidget {
+  const TextCustom({
+    super.key,
+    required this.text,
+    required this.color,
+    required this.textStyle,
+    this.fontSizeCustom,
+    this.fontHeightCustom,
+    this.fontWeightCustom,
+    this.fontLetterSpacingCustom,
+    this.maxLines = 1,
+    this.textAlignCustom = TextAlign.start,
+  });
+
+  final String text;
+  final Color color;
+  final TextStyle textStyle;
+  final double? fontSizeCustom;
+  final double? fontHeightCustom;
+  final FontWeight? fontWeightCustom;
+  final double? fontLetterSpacingCustom;
+  final int? maxLines;
+  final TextAlign? textAlignCustom;
+
+  @override
+  Widget build(BuildContext context) {
+    var fontSizeFinal = fontSizeCustom ?? textStyle.fontSize;
+    var textHeightFinal = fontHeightCustom ?? textStyle.height;
+    return SizedBox(
+      // color: Colors.yellow,
+      height: fontSizeFinal!.h * textHeightFinal! * maxLines!,
+      child: Text(
+        text,
+        maxLines: maxLines,
+        overflow: TextOverflow.ellipsis,
+        textAlign: textAlignCustom,
+        textScaler: TextScaler.noScaling,
+        style: textStyle.copyWith(
+          color: color,
+          fontSize: fontSizeFinal.h,
+          height: fontHeightCustom,
+          fontWeight: fontWeightCustom ?? textStyle.fontWeight,
+          letterSpacing: fontLetterSpacingCustom ?? textStyle.letterSpacing,
+        ),
+      ),
+    );
+  }
+}

a73668b1464f83cfeff4bf378e7b5d6e9e6ad827
Yasin Bilgin <stdxpe@gmail.com>
2024-07-21 15:49:29 +0300

    shopping cart screen created

---
+++ lib/ui/screens/shopping_cart_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:ecommerce_shopping_project/models/dummy_product_short_list.dart';
+import 'package:ecommerce_shopping_project/ui/widgets/app_bar_main.dart';
+import 'package:ecommerce_shopping_project/ui/widgets/listviews_and_gridviews/vertical_listview_product_card_horizontal_detailed.dart';
+import 'package:ecommerce_shopping_project/ui/widgets/titles/title_main.dart';
+import 'package:ecommerce_shopping_project/utilities/utilities_library_imports.dart';
+
+class ShoppingCartScreen extends StatelessWidget {
+  const ShoppingCartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const AppBarMain(),
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          physics: const ClampingScrollPhysics(),
+          children: [
+            Container(
+              // color: Colors.green.withOpacity(0.5),
+              child: const TitleMain(
+                title: AppStrings.shoppingCartScreenTitle,
+                itemCount: 13,
+              ),
+            ),
+            SizedBox(height: 57.h),
+            VerticalListviewProductCardHorizontalDetailed(
+              isCardElevated: false,
+              productsList: dummyProductShortList,
+              cardHeight: 250,
+              paddingMain: Constants.kMainPaddingHorizontal,
+              paddingBetweenElements: Constants.kMainSpacingBTWCardsVertical,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

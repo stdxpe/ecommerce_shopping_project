84f93ce31c47c1931765047a3fc904532345df47
Yasin Bilgin <stdxpe@gmail.com>
2024-07-21 14:21:52 +0300

    boxshadow and foreground paint feature added

--- lib/ui/widgets/text_custom.dart
+++ lib/ui/widgets/text_custom.dart
@@ -13,6 +13,8 @@ class TextCustom extends StatelessWidget {
     this.fontLetterSpacingCustom,
     this.maxLines = 1,
     this.textAlignCustom = TextAlign.start,
+    this.boxShadowsCustom,
+    this.foregroundPaintCustom,
   });
 
   final String text;
@@ -24,13 +26,17 @@ class TextCustom extends StatelessWidget {
   final double? fontLetterSpacingCustom;
   final int? maxLines;
   final TextAlign? textAlignCustom;
+  final List<BoxShadow>? boxShadowsCustom;
+  final Paint? foregroundPaintCustom;
 
   @override
   Widget build(BuildContext context) {
     var fontSizeFinal = fontSizeCustom ?? textStyle.fontSize;
     var textHeightFinal = fontHeightCustom ?? textStyle.height;
-    return SizedBox(
+    return Container(
       // color: Colors.yellow,
+      padding: EdgeInsets.zero,
+
       height: fontSizeFinal!.h * textHeightFinal! * maxLines!,
       child: Text(
         text,
@@ -39,11 +45,15 @@ class TextCustom extends StatelessWidget {
         textAlign: textAlignCustom,
         textScaler: TextScaler.noScaling,
         style: textStyle.copyWith(
-          color: color,
+          color: (foregroundPaintCustom != null) ? null : color,
           fontSize: fontSizeFinal.h,
           height: fontHeightCustom,
           fontWeight: fontWeightCustom ?? textStyle.fontWeight,
           letterSpacing: fontLetterSpacingCustom ?? textStyle.letterSpacing,
+          shadows: [
+            if (boxShadowsCustom != null) ...boxShadowsCustom!,
+          ],
+          foreground: foregroundPaintCustom,
         ),
       ),
     );

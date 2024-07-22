556324f9d1f4144e7398cdeff9e5314b9c5af679
Yasin Bilgin <stdxpe@gmail.com>
2024-07-22 21:28:43 +0300

    temp review model class created

---
+++ lib/models/review.dart
@@ -0,0 +1,15 @@
+class Review {
+  final String senderName;
+  final String senderPhoto;
+  final double productRating;
+  final String createdAt;
+  final String text;
+
+  Review({
+    required this.senderName,
+    required this.senderPhoto,
+    required this.productRating,
+    required this.createdAt,
+    required this.text,
+  });
+}

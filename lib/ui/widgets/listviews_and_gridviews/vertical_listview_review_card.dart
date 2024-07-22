d2f617ecfa5291700094a097222d0cc7270f3fbc
Yasin Bilgin <stdxpe@gmail.com>
2024-07-22 21:29:06 +0300

    listview of review cards created vertically

---
+++ lib/ui/widgets/listviews_and_gridviews/vertical_listview_review_card.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:ecommerce_shopping_project/models/review.dart';
+import 'package:ecommerce_shopping_project/ui/widgets/cards/review_card.dart';
+
+class VerticalListviewReviewCard extends StatelessWidget {
+  const VerticalListviewReviewCard({
+    super.key,
+    required this.reviewsList,
+    required this.paddingMain,
+    required this.paddingBetweenElements,
+  });
+
+  final List<Review> reviewsList;
+
+  final double paddingMain;
+  final double paddingBetweenElements;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: ListView.builder(
+        shrinkWrap: true,
+        padding: EdgeInsets.symmetric(horizontal: paddingMain.w),
+        physics: const NeverScrollableScrollPhysics(),
+        clipBehavior: Clip.none,
+        itemCount: reviewsList.length,
+        scrollDirection: Axis.vertical,
+        itemBuilder: (context, index) {
+          var currentItem = reviewsList[index];
+          return Padding(
+            padding: EdgeInsets.only(bottom: paddingBetweenElements.h),
+            child: ReviewCard(
+              review: Review(
+                senderName: currentItem.senderName,
+                senderPhoto: currentItem.senderPhoto,
+                productRating: currentItem.productRating,
+                createdAt: currentItem.createdAt,
+                text: currentItem.text,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}

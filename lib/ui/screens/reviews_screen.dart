c39fcd698ff82d4ba7255e365044ab3842397d7f
Yasin Bilgin <stdxpe@gmail.com>
2024-07-22 21:27:56 +0300

    reviews screen created

--- lib/ui/screens/reviews_screen.dart
+++ lib/ui/screens/reviews_screen.dart
@@ -1,21 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:rating_summary/rating_summary.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:ecommerce_shopping_project/models/dummy_reviews_list.dart';
+import 'package:ecommerce_shopping_project/models/product.dart';
+import 'package:ecommerce_shopping_project/ui/widgets/cards/product_card_horizontal_mini.dart';
+import 'package:ecommerce_shopping_project/ui/widgets/listviews_and_gridviews/vertical_listview_review_card.dart';
+import 'package:ecommerce_shopping_project/ui/widgets/switches/switch_rating_summary.dart';
+import 'package:ecommerce_shopping_project/utilities/utilities_library_imports.dart';
 
 class ReviewsScreen extends StatelessWidget {
-  const ReviewsScreen({super.key});
+  const ReviewsScreen({super.key, required this.product});
 
-// https://pub.dev/packages/rating_summary
+  final Product product;
+
   @override
   Widget build(BuildContext context) {
-    return RatingSummary(
-      counter: 13,
-      average: 3.846,
-      showAverage: true,
-      counterFiveStars: 5,
-      counterFourStars: 4,
-      counterThreeStars: 2,
-      counterTwoStars: 1,
-      counterOneStars: 1,
+    return Scaffold(
+      // appBar: AppBarMain(),
+      body: SafeArea(
+        bottom: false,
+        child: ListView(
+          padding: EdgeInsets.symmetric(
+            horizontal: Constants.kMainPaddingHorizontal.w,
+          ),
+          children: [
+            ProductCardHorizontalMini(
+              product: product,
+              isCardElevated: true,
+              cardWidth: 1.sw,
+              cardHeight: 200,
+            ),
+            SizedBox(height: 100.h),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 55.w),
+              child: const SwitchRatingSummary(),
+            ),
+            SizedBox(height: 100.h),
+            VerticalListviewReviewCard(
+              reviewsList: dummyReviewsList,
+              paddingMain: 0,
+              paddingBetweenElements: Constants.kMainSpacingBTWCardsVertical.h,
+            ),
+            SizedBox(height: 100.h),
+          ],
+        ),
+      ),
     );
   }
 }

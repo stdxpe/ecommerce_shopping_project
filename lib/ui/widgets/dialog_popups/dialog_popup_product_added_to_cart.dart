1c30c62cde31b7c45f60a98bf8d7d002927ccf43
Yasin Bilgin <stdxpe@gmail.com>
2024-07-25 15:25:39 +0300

    dialog popup of details screen created

---
+++ lib/ui/widgets/dialog_popups/dialog_popup_product_added_to_cart.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:ecommerce_shopping_project/ui/widgets/buttons/button_main.dart';
+import 'package:ecommerce_shopping_project/ui/widgets/text_custom.dart';
+import 'package:ecommerce_shopping_project/utilities/utilities_library_imports.dart';
+
+class DialogPopupProductAddedToCart extends StatelessWidget {
+  final String imageUrl;
+  final double cardHeight;
+  final double cardWidth;
+
+  const DialogPopupProductAddedToCart({
+    required this.imageUrl,
+    required this.cardHeight,
+    required this.cardWidth,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: cardWidth,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(
+            height: cardHeight * 0.5,
+            width: cardWidth,
+            imageUrl,
+            fit: BoxFit.cover,
+            alignment: Alignment.topCenter,
+          ),
+          SizedBox(height: Constants.kDialogPopupSpacingBTWTextVertical.h),
+          TextCustom(
+            text: AppStrings
+                .productDetailsScreenDialogPopupAddedToShoppingCartMessage,
+            textStyle: context.textTheme.displaySmall!,
+            color: context.colorPalette.text,
+            maxLines: 2,
+            fontSizeCustom: 46,
+            fontWeightCustom: FontWeight.w600,
+            fontHeightCustom: 1.0,
+            fontLetterSpacingCustom: 0,
+            textAlignCustom: TextAlign.center,
+          ),
+          SizedBox(height: Constants.kDialogPopupSpacingBTWTextVertical.h),
+          ButtonMain(
+            onPressed: () {},
+            text: AppStrings.productDetailsScreenPopupButtonGoToShoppingCart,
+            paddingHorizontal: Constants.kDialogPopupPaddingButtonHorizontal.w,
+            paddingVertical: 0,
+          ),
+          SizedBox(height: Constants.kDialogPopupSpacingBTWButtonsVertical.h),
+          ButtonMain(
+            onPressed: () {},
+            text: AppStrings.productDetailsScreenPopupButtonContinueShopping,
+            backgroundColor: context.colorPalette.buttonMainBackgroundSecondary,
+            foregroundColor: context.colorPalette.buttonMainForegroundSecondary,
+            paddingHorizontal: Constants.kDialogPopupPaddingButtonHorizontal.w,
+            paddingVertical: 0,
+          ),
+          SizedBox(height: Constants.kDialogPopupPaddingButtonBottom.h),
+        ],
+      ),
+    );
+  }
+}

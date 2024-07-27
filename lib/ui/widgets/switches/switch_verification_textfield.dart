a8d153eb5f650d0fd6631f03a4d08c5c5d8d18e4
Yasin Bilgin <stdxpe@gmail.com>
2024-07-27 14:17:03 +0300

    verification code textfield created

---
+++ lib/ui/widgets/switches/switch_verification_textfield.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'package:ecommerce_shopping_project/ui/widgets/switches/switch_verification_code_dots.dart';
+import 'package:ecommerce_shopping_project/ui/widgets/textformfield_main.dart';
+
+class SwitchVerificationTextField extends StatelessWidget {
+  const SwitchVerificationTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          // color: Colors.red.withOpacity(0.5),
+          child: const TextformfieldMain(
+            autoFocus: true,
+            text: '',
+            textInputType: TextInputType.number,
+            lineColor: Colors.transparent,
+            textColor: Colors.transparent,
+          ),
+        ),
+        const SwitchVerificationCodeDots(
+          currentIndex: 1,
+          itemCount: 4,
+        ),
+      ],
+    );
+  }
+}

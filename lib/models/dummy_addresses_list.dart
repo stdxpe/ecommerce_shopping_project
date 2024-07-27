0f8cd77820d817e46ecf205ea89f718ad94d53fc
Yasin Bilgin <stdxpe@gmail.com>
2024-07-27 15:43:43 +0300

    dummy address and credit card lists created

---
+++ lib/models/dummy_addresses_list.dart
@@ -0,0 +1,31 @@
+const List dummyAddressesList = [
+  {
+    'addressTitle': 'Home Address',
+    'address': '21st Greenday Street 10021\nNew York Manhattan\nUnited States',
+  },
+  {
+    'addressTitle': 'Work Address',
+    'address': '39th Avenue 9021\nNew York Manhattan\nUnited States',
+  },
+  {
+    'addressTitle': 'Jane\'s Home',
+    'address': '100th Greenday Street 10021\nNew York Manhattan\nUnited States',
+  },
+];
+
+const List dummyCreditCardsList = [
+  {
+    'cardTitle': 'My American Express',
+    'cardHolder': 'Jane Doe',
+    'cardNumber': '5555 5555 5555 5555',
+    'expDate': '12/23',
+    'cvv': '123',
+  },
+  {
+    'cardTitle': 'My Master Card',
+    'cardHolder': 'David Doe',
+    'cardNumber': '4444 5555 5555 5555',
+    'expDate': '12/23',
+    'cvv': '123',
+  },
+];

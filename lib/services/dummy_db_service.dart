4fd1da4f17d7d157ff811cb85ebe085c7ddea0d2
Yasin Bilgin <stdxpe@gmail.com>
2024-07-31 18:59:21 +0300

    dummy db service getAllProducts method created

--- lib/services/dummy_db_service.dart
+++ lib/services/dummy_db_service.dart
@@ -1,11 +1,17 @@
+import 'package:ecommerce_shopping_project/models/dummy_data/dummy_products.dart';
 import 'package:ecommerce_shopping_project/models/product.dart';
 import 'package:ecommerce_shopping_project/services/i_db_service.dart';
 
 class DummyDbService extends IDbService {
   @override
-  Future<List<Product>> getAllProducts() {
-    // TODO: implement getAllProducts
-    throw UnimplementedError();
+  Future<List<Product>> getAllProducts() async {
+    print('DummyDbService getAllProducts() Executed: "Waiting 2 seconds...');
+    await Future.delayed(const Duration(seconds: 2));
+    List<Product> productsList = dummyProducts;
+
+    /// TODO: FromJSON methods here.
+    /// dummyProductJsonList.map((e) => Product.fromJson(e)).toList();
+    return await Future.value(productsList);
   }
 
   @override

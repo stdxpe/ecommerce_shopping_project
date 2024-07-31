07b324cebd9b77fad3d1dd2657612caa1c14763d
Yasin Bilgin <stdxpe@gmail.com>
2024-07-31 18:54:47 +0300

    dummy db service registered to locator

--- lib/services/dependency_injection_service.dart
+++ lib/services/dependency_injection_service.dart
@@ -1,7 +1,10 @@
+import 'package:ecommerce_shopping_project/services/dummy_db_service.dart';
+import 'package:ecommerce_shopping_project/services/i_db_service.dart';
 import 'package:get_it/get_it.dart';
 
 final locator = GetIt.instance;
 
 void locatorGetItSetup() {
   /// Database Services (Data Access Layer)
+  locator.registerLazySingleton<IDbService>(() => DummyDbService());
 }

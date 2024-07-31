ded0f0603481eb78b3c86b1d3f4b496465af70ff
Yasin Bilgin <stdxpe@gmail.com>
2024-07-31 19:08:22 +0300

    dummy db manager getAllProducts method created

--- lib/business/dummy_db_manager.dart
+++ lib/business/dummy_db_manager.dart
@@ -1,11 +1,16 @@
 import 'package:ecommerce_shopping_project/business/i_db_repository.dart';
 import 'package:ecommerce_shopping_project/models/product.dart';
+import 'package:ecommerce_shopping_project/services/dependency_injection_service.dart';
+import 'package:ecommerce_shopping_project/services/i_db_service.dart';
 
 class DummyDbManager extends IDBRepository {
+  var _dbService = locator<IDbService>();
+
   @override
   Future<List<Product>> getAllProducts() {
-    // TODO: implement getAllProducts
-    throw UnimplementedError();
+    print('DummyDbManager getAllProducts() Executed: "Waiting 2 seconds..."');
+    var allProducts = _dbService.getAllProducts();
+    return allProducts;
   }
 
   @override

35735df69134d2d35be2af23bd394011d2f554fb
Yasin Bilgin <stdxpe@gmail.com>
2024-08-03 23:32:35 +0300

    former bottomnavbar package leftovers deleted

--- lib/ui/test_screens/shimmer_screen.dart
+++ lib/ui/test_screens/shimmer_screen.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
-import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 import 'package:shimmer/shimmer.dart';
 
 import 'package:ecommerce_shopping_project/models/dummy_data/dummy_products.dart';
-import 'package:ecommerce_shopping_project/ui/screens/payment_screen_pageview.dart';
 import 'package:ecommerce_shopping_project/ui/widgets/%20bottom_sheets/bottom_sheet_buttons_shopping_cart.dart';
 import 'package:ecommerce_shopping_project/ui/widgets/app_bars/app_bar_main.dart';
 import 'package:ecommerce_shopping_project/ui/widgets/listviews_and_gridviews/vertical_listview_product_card_horizontal_detailed.dart';
@@ -66,14 +64,7 @@ class ShimmerScreen extends StatelessWidget {
               Align(
                 alignment: Alignment.bottomCenter,
                 child: BottomSheetButtonsShoppingCart(
-                  onPressed: () {
-                    PersistentNavBarNavigator.pushNewScreen(
-                      pageTransitionAnimation: PageTransitionAnimation.fade,
-                      context,
-                      screen: const PaymentScreenPageview(),
-                      withNavBar: false,
-                    );
-                  },
+                  onPressed: () {},
                   totalAmount: 210.99,
                   shippingFee: 5.99,
                 ),

b2a3263d15b5c1aa4ca06fdb28719f6394280bac
Yasin Bilgin <stdxpe@gmail.com>
2024-08-05 12:38:41 +0300

    getWishlistProducts() and getShoppingCartProducts() methods created

--- lib/services/i_db_service.dart
+++ lib/services/i_db_service.dart
@@ -1,18 +1,25 @@
+import 'package:ecommerce_shopping_project/models/collection.dart';
 import 'package:ecommerce_shopping_project/models/product.dart';
 
 abstract class IDbService {
   Future<List<Product>> getAllProducts();
   Future<Product> getProductById({required String id});
   Future<List<Product>> getProductsByFilter({required String filter});
+
+  Future<List<Product>> getWishlistProducts();
+  Future<void> addProductToWishlist({required String productId});
+  Future<void> deleteProductFromWishlist({required String productId});
+
+  Future<List<Product>> getShoppingCartProducts();
+  Future<void> addProductToShoppingCart({required String productId});
+  Future<void> deleteProductFromShoppingCart({required String productId});
 }
 
-// CRUD
 
-// getWishlistProducts()
-// addProductToWishlist({required String id})
-// deleteProductFromWishlist({required String id})
+// Future<List<Collection>> getAllCollections();
+// Future<List<Collection>> getCollectionById({required String id});
+// /// TODO: To-be Personal Collections, 'Recommended For You' Section. Above method to-be-called.
 
-// getCartProducts()
-// addProductToCart({required String id})
-// deleteProductFromCart({required String id})
+
+
 

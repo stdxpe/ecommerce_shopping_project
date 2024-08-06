969e94147e1b42b1d8d094e9734a12b77cdad7aa
Yasin Bilgin <stdxpe@gmail.com>
2024-08-07 00:16:10 +0300

    order_product listviews created

---
+++ lib/ui/widgets/cards/order_product_card_horizontal_detailed.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+
+import 'package:ecommerce_shopping_project/models/order_product.dart';
+import 'package:ecommerce_shopping_project/services/navigation_service.dart';
+import 'package:ecommerce_shopping_project/ui/widgets/text_custom.dart';
+import 'package:ecommerce_shopping_project/utilities/utilities_library_imports.dart';
+
+class OrderProductCardHorizontalDetailed extends StatelessWidget {
+  const OrderProductCardHorizontalDetailed({
+    super.key,
+    required this.orderProduct,
+    required this.cardHeight,
+    this.isCardElevated = false,
+  });
+
+  final OrderProduct orderProduct;
+  final double cardHeight;
+  final bool? isCardElevated;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: GestureDetector(
+        onTap: () => context.push(Routes.productDetails, extra: orderProduct),
+        child: Container(
+          height: cardHeight.h,
+          decoration: BoxDecoration(
+            color: isCardElevated!
+                ? context.colorPalette.cardBackground
+                : Colors.transparent,
+            borderRadius: BorderRadius.circular(
+              Constants.kRadiusCardPrimary.r,
+            ),
+            boxShadow: [
+              if (isCardElevated == true)
+                BoxShadows.kBoxShadowProductCard(
+                  color: context.colorPalette.shadowPrimary,
+                ),
+            ],
+          ),
+          child: Row(
+            children: [
+              /// CARD IMAGE
+              Container(
+                height: cardHeight.h,
+                width: cardHeight.h,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(
+                      orderProduct.selectedProduct.mainPhoto,
+                    ),
+                  ),
+                  borderRadius: BorderRadius.circular(
+                    Constants.kRadiusCardPrimary.r,
+                  ),
+                  boxShadow: [
+                    if (isCardElevated == false)
+                      BoxShadows.kBoxShadowImage(
+                        color: context.colorPalette.shadowPrimary,
+                      ),
+                  ],
+                ),
+              ),
+
+              /// CARD TEXTS
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.symmetric(
+                    vertical:
+                        Constants.kHorizontalCardDetailedPaddingVertical.h,
+                    horizontal:
+                        Constants.kHorizontalCardDetailedPaddingHorizontal.w,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextCustom(
+                            text: orderProduct.selectedProduct.title,
+                            textStyle: context.textTheme.bodyLarge!,
+                            color: context.colorPalette.cardTextPrimary,
+                            maxLines: 2,
+                            isHeightConstraintRelated: false,
+                          ),
+                          SizedBox(
+                            height: Constants
+                                .kHorizontalCardDetailedSpacingBTWItemsVertical
+                                .h,
+                          ),
+                          TextCustom(
+                            text:
+                                '\$${orderProduct.selectedProduct.price.toStringAsFixed(2)}',
+                            textStyle: context.textTheme.bodyMedium!,
+                            color: context.colorPalette.cardTextSecondary,
+                          ),
+                        ],
+                      ),
+                      TextCustom(
+                        text:
+                            'Size: ${orderProduct.selectedSize}  |  Color: ${orderProduct.selectedColor}',
+                        textStyle: context.textTheme.bodySmall!,
+                        color: context.colorPalette.cardTextTertiary,
+                        fontWeightCustom: FontWeight.w400,
+                        fontSizeCustom: 32,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/widgets/listviews_and_gridviews/vertical_listview_order_product_card_horizontal.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:ecommerce_shopping_project/models/order_product.dart';
+import 'package:ecommerce_shopping_project/ui/widgets/cards/order_product_card_horizontal_detailed.dart';
+import 'package:ecommerce_shopping_project/ui/widgets/minor_widgets/dismissible_delete_widget.dart';
+import 'package:ecommerce_shopping_project/ui/widgets/placeholders/card_placeholder_horizontal.dart';
+import 'package:ecommerce_shopping_project/ui/widgets/switches/switch_item_counter.dart';
+import 'package:ecommerce_shopping_project/utilities/utilities_library_imports.dart';
+
+class VerticalListviewOrderProductCardHorizontal extends StatelessWidget {
+  const VerticalListviewOrderProductCardHorizontal({
+    super.key,
+    required this.orderProductsList,
+    required this.cardHeight,
+    this.isCardElevated = true,
+    required this.paddingMain,
+    required this.paddingBetweenElements,
+    this.dismissibleEnabled = true,
+    this.useShimmer = false,
+  });
+
+  final List<OrderProduct> orderProductsList;
+  final double cardHeight;
+  final bool? isCardElevated;
+  final double paddingMain;
+  final double paddingBetweenElements;
+  final bool? dismissibleEnabled;
+  final bool? useShimmer;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: ListView.builder(
+        padding: EdgeInsets.zero,
+        shrinkWrap: true,
+        physics: const ClampingScrollPhysics(),
+        clipBehavior: Clip.none,
+        itemCount: orderProductsList.length,
+        scrollDirection: Axis.vertical,
+        itemBuilder: (context, index) {
+          return DismissibleDeleteWidget(
+            // TODO: UNIQUE KEY TO BE CHANGED with Product ID's.
+            // TODO: orderProductsList[index].id,
+            uniqueKey: '$index',
+            dismissibleEnabled: dismissibleEnabled,
+            child: Padding(
+              padding: EdgeInsets.only(
+                bottom: paddingBetweenElements.w / 2,
+                top: paddingBetweenElements.w / 2,
+                left: paddingMain.w,
+                right: paddingMain.w,
+              ),
+              child: useShimmer!
+                  ? CardPlaceholderHorizontal(
+                      cardHeight: cardHeight,
+                      cardWidth: context.mediaQuery.size.width,
+                    )
+                  : Stack(
+                      children: [
+                        OrderProductCardHorizontalDetailed(
+                          orderProduct: orderProductsList[index],
+                          isCardElevated: isCardElevated,
+                          cardHeight: cardHeight,
+                        ),
+                        Positioned.fill(
+                          right: 25.w,
+                          bottom: 10.h,
+                          child: Align(
+                            alignment: Alignment.bottomRight,
+                            child: SwitchItemCounter(
+                              itemCount: orderProductsList[index].itemCount,
+                              onPressedMinus: () {},
+                              // ref.read(shoppingCartProvider.notifier).decreaseItemCounter(index),
+                              onPressedPlus: () {},
+                              // ref.read(shoppingCartProvider.notifier).increaseItemCounter(index),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}

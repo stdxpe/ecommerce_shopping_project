a2d7febfe262d41c4c056e91dbbd21fee61e2c7f
Yasin Bilgin <stdxpe@gmail.com>
2024-08-11 15:38:41 +0300

    comment lines deleted

--- lib/ui/riverpod_providers/feature_selector_providers.dart
+++ lib/ui/riverpod_providers/feature_selector_providers.dart
@@ -30,51 +30,6 @@ final featureSelectorOnPressedProvider =
   /// TODO: Else error message
 });
 
-// final featureSelectorProvider =
-//     StateNotifierProvider<FeatureSelectorNotifier, void>((ref) {
-//   return FeatureSelectorNotifier(ref: ref);
-// });
-
-// class FeatureSelectorNotifier extends StateNotifier<void> {
-//   FeatureSelectorNotifier({required this.ref}) : super(null);
-//   final StateNotifierProviderRef ref;
-
-//   addToCart(BuildContext context, Product product) async {
-//     String selectedColor = product.colors[ref.watch(colorSelectorProvider)];
-//     String selectedSize = product.sizes[ref.watch(sizeSelectorProvider)];
-//     context.findRootAncestorStateOfType<NavigatorState>()?.pop();
-
-//     var result =
-//         await ref.read(shoppingCartProvider.notifier).addProductToShoppingCart(
-//               cartProduct: CartProduct(
-//                 id: product.id,
-//                 selectedProduct: product,
-//                 selectedColor: selectedColor,
-//                 selectedSize: selectedSize,
-//                 itemCount: 1,
-//               ),
-//             );
-
-// //  final NavigatorState navigator = rootNavigator
-// //         ? context.findRootAncestorStateOfType<NavigatorState>()
-// //         : context.findAncestorStateOfType<NavigatorState>();
-//     // final NavigatorState navigator =
-//     //     context.findRootAncestorStateOfType<NavigatorState>()!;
-
-//     // navigator.context.pop();
-
-//     // context.findRootAncestorStateOfType<NavigatorState>()?.pop();
-
-//     if (result) {
-//       // ref.read(goRouterProvider).pop();
-
-//       ref
-//           .read(dialogPopupProvider.notifier)
-//           .addedToCart(context: context, product: product);
-//     }
-//   }
-// }
-
 final sizeSelectorProvider = StateProvider<int>((ref) {
   int currentIndex = 0;
   return currentIndex;

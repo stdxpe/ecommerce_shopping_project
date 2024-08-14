fa3e26f232581117968956e9bf8a9c7b0618287d
Yasin Bilgin <stdxpe@gmail.com>
2024-08-14 22:25:10 +0300

    firebase auth implemented to project

---
+++ lib/ui/riverpod_providers/firebase/firebase_auth_provider.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final firebaseAuthProvider =
+    Provider<FirebaseAuth>((ref) => FirebaseAuth.instance);

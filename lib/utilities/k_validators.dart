f65337eae0c645dbb6ee11b528fc1dbeeaf18f8c
Yasin Bilgin <stdxpe@gmail.com>
2024-08-14 12:34:52 +0300

    validator extensions created for valid email or password

---
+++ lib/utilities/k_validators.dart
@@ -0,0 +1,11 @@
+extension Validator on String {
+  bool isValidEmail() => RegExp(
+          r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+      .hasMatch(this);
+
+  bool isValidPassword() =>
+      RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?!.*\W).{8,30}$')
+          .hasMatch(this);
+
+  // TODO: Credit Card Number Validator
+}

04b69c8a3cc0d7eabe4bd7e6b1b2b0d691cf1695
Yasin Bilgin <stdxpe@gmail.com>
2024-09-01 14:51:40 +0300

    order methods refactored

--- lib/business/order_manager.dart
+++ lib/business/order_manager.dart
@@ -1,14 +1,12 @@
-import 'package:ecommerce_shopping_project/models/order.dart' as model;
-import 'package:ecommerce_shopping_project/models/order_product.dart';
-import 'package:ecommerce_shopping_project/models/product.dart';
-import 'package:ecommerce_shopping_project/models/user_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:uuid/uuid.dart';
 
+import 'package:ecommerce_shopping_project/models/order.dart' as model;
 import 'package:ecommerce_shopping_project/business/abstract_classes/i_order_repository.dart';
+import 'package:ecommerce_shopping_project/models/order_product.dart';
+import 'package:ecommerce_shopping_project/models/user_model.dart';
 import 'package:ecommerce_shopping_project/services/abstract_classes/i_user_service.dart';
 import 'package:ecommerce_shopping_project/services/global_services/dependency_injection_service.dart';
-
-import 'package:uuid/uuid.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class OrderManager extends IOrderRepository {
   final _userService = locator<IUserService>();
@@ -28,7 +26,7 @@ class OrderManager extends IOrderRepository {
   }
 
   @override
-  Future<void> addOrder({
+  Future<void> createOrder({
     required List<OrderProduct> products,
     required UserModel userModel,
   }) async {
@@ -61,32 +59,33 @@ class OrderManager extends IOrderRepository {
     }
   }
 
-  @override
-  Future<OrderProduct> createOrderProduct(
-      {required Product product,
-      required String selectedColor,
-      required String selectedSize,
-      required int itemCount}) async {
-    try {
-      print('OrderManager createOrderProduct try block exec');
-      return OrderProduct(
-        id: const Uuid().v4(),
-        selectedProductId: product.id,
-        title: product.title,
-        photo: product.mainPhoto,
-        price: product.price,
-        selectedColor: selectedColor,
-        selectedSize: selectedSize,
-        itemCount: itemCount,
-      );
-    } on Exception catch (_) {
-      print(
-          'OrderManager createOrderProduct catch exception block exec, rethrowing');
-      rethrow;
-    } on Error catch (_) {
-      print(
-          'OrderManager createOrderProduct catch error block exec, rethrowing');
-      rethrow;
-    }
-  }
+  /// TODO: Problematic?
+  // @override
+  // Future<OrderProduct> createOrderProduct(
+  //     {required Product product,
+  //     required String selectedColor,
+  //     required String selectedSize,
+  //     required int itemCount}) async {
+  //   try {
+  //     print('OrderManager createOrderProduct try block exec');
+  //     return OrderProduct(
+  //       id: const Uuid().v4(),
+  //       selectedProductId: product.id,
+  //       title: product.title,
+  //       photo: product.mainPhoto,
+  //       price: product.price,
+  //       selectedColor: selectedColor,
+  //       selectedSize: selectedSize,
+  //       itemCount: itemCount,
+  //     );
+  //   } on Exception catch (_) {
+  //     print(
+  //         'OrderManager createOrderProduct catch exception block exec, rethrowing');
+  //     rethrow;
+  //   } on Error catch (_) {
+  //     print(
+  //         'OrderManager createOrderProduct catch error block exec, rethrowing');
+  //     rethrow;
+  //   }
+  // }
 }

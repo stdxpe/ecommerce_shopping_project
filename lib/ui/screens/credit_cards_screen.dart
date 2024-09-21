3a7bda324045bb0dbd0ad64f801c890aa1dde6b2
Yasin Bilgin <stdxpe@gmail.com>
2024-09-21 20:49:18 +0300

    credit cards screen updated according to providers

--- lib/ui/screens/credit_cards_screen.dart
+++ lib/ui/screens/credit_cards_screen.dart
@@ -3,10 +3,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 
-import 'package:ecommerce_shopping_project/services/dummy_data/dummy_addresses_list.dart';
 import 'package:ecommerce_shopping_project/services/global_services/navigation_service.dart';
-import 'package:ecommerce_shopping_project/ui/widgets/app_bars/app_bar_main.dart';
+import 'package:ecommerce_shopping_project/ui/riverpod_providers/credit_card_providers.dart';
 import 'package:ecommerce_shopping_project/ui/widgets/listviews_and_gridviews/vertical_listview_profile_credit_cards.dart';
+import 'package:ecommerce_shopping_project/ui/widgets/minor_widgets/error_occured_widget.dart';
+import 'package:ecommerce_shopping_project/ui/widgets/minor_widgets/no_items_found_widget.dart';
 import 'package:ecommerce_shopping_project/ui/widgets/titles/title_main.dart';
 import 'package:ecommerce_shopping_project/utilities/utilities_library_imports.dart';
 
@@ -16,10 +17,6 @@ class CreditCardsScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: const AppBarMain(
-        automaticallyImplyLeading: true,
-        useSearchButton: false,
-      ),
       body: SafeArea(
         child: ListView(
           padding: EdgeInsets.zero,
@@ -27,13 +24,28 @@ class CreditCardsScreen extends ConsumerWidget {
           children: [
             TitleMain(
               title: AppStrings.creditCardsScreenTitle,
+              enableTitleAsBackButton: true,
               icon: Icons.add,
+              paddingTop: 85.h,
               paddingHorizontal: Constants.kMainPaddingHorizontal.w,
-              onPressed: () => context.push(Routes.bottomSheetCreditCards),
+              onPressed: () {
+                ref.read(selectedCreditCardIndex.notifier).state =
+                    ref.read(creditCardsRawList).length - 1;
+                context.push(Routes.bottomSheetCreditCards);
+              },
             ),
-            const VerticalListviewProfileCreditCards(
-              cardsList: dummyCreditCardsList,
-            ),
+            ref.watch(creditCardsProvider).hasError
+                ? const ErrorOccuredWidget()
+                : (ref.watch(creditCardsProvider).value != null &&
+                        ref.watch(creditCardsProvider).value!.isNotEmpty)
+                    ? ref.watch(creditCardsProvider).when(
+                        loading: () => const Center(
+                            child: Center(child: CircularProgressIndicator())),
+                        error: (error, stackTrace) =>
+                            const ErrorOccuredWidget(),
+                        data: (data) =>
+                            VerticalListviewProfileCreditCards(cards: data))
+                    : const NoItemsFoundWidget(),
             SizedBox(height: 100.h),
           ],
         ),

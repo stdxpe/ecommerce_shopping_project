bfb009650bd22a01f96b1d2cd89650057e77f6c4
Yasin Bilgin <stdxpe@gmail.com>
2024-09-23 00:49:05 +0300

    onPressed method of gestureDetector widget updated on saved addresses listview

--- lib/ui/widgets/listviews_and_gridviews/vertical_listview_profile_addresses.dart
+++ lib/ui/widgets/listviews_and_gridviews/vertical_listview_profile_addresses.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 import 'package:ecommerce_shopping_project/models/address.dart';
 import 'package:ecommerce_shopping_project/services/global_services/navigation_service.dart';
+import 'package:ecommerce_shopping_project/ui/riverpod_providers/shipping_addresses_providers.dart';
 import 'package:ecommerce_shopping_project/ui/widgets/titles/title_address_section.dart';
 
-class VerticalListviewProfileAddresses extends StatelessWidget {
+class VerticalListviewProfileAddresses extends ConsumerWidget {
   const VerticalListviewProfileAddresses({super.key, required this.addresses});
 
   final List<Address> addresses;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return ListView.builder(
       physics: const NeverScrollableScrollPhysics(),
       padding: EdgeInsets.zero,
@@ -20,8 +22,11 @@ class VerticalListviewProfileAddresses extends StatelessWidget {
       scrollDirection: Axis.vertical,
       itemBuilder: (context, index) {
         return TitleAddressSection(
-          onPressed: () => context.push(Routes.bottomSheetAddresses,
-              extra: addresses[index]),
+          onPressed: () {
+            ref.read(disposeAddressControllers);
+            ref.read(selectedAddressIndex.notifier).state = index;
+            context.push(Routes.bottomSheetAddresses);
+          },
           useTopDivider: index == 0 ? true : false,
           title: addresses[index].addressTitle,
           addressText: addresses[index].addressText,

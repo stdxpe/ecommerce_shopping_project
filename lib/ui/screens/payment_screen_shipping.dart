75c52f41f8093a99b041b22c9c7381893e2e8d1f
Yasin Bilgin <stdxpe@gmail.com>
2024-09-23 00:44:57 +0300

    payment shipping screen turned into consumer stateful widget and ui updated according to the new providers

--- lib/ui/screens/payment_screen_shipping.dart
+++ lib/ui/screens/payment_screen_shipping.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
@@ -8,146 +9,210 @@ import 'package:ecommerce_shopping_project/ui/riverpod_providers/payment_screen_
 import 'package:ecommerce_shopping_project/ui/riverpod_providers/shipping_addresses_providers.dart';
 import 'package:ecommerce_shopping_project/ui/widgets/bottom_sheets/bottom_sheet_buttons_payment_shipping.dart';
 import 'package:ecommerce_shopping_project/ui/widgets/bottom_sheets/bottom_sheet_buttons_profile_update_or_delete.dart';
+import 'package:ecommerce_shopping_project/ui/widgets/listviews_and_gridviews/horizontal_listview_address_chips.dart';
 import 'package:ecommerce_shopping_project/ui/widgets/switches/switch_checkbox_main.dart';
 import 'package:ecommerce_shopping_project/ui/widgets/text_custom.dart';
 import 'package:ecommerce_shopping_project/ui/widgets/textformfield_main.dart';
 import 'package:ecommerce_shopping_project/ui/widgets/titles/title_main.dart';
+import 'package:ecommerce_shopping_project/utilities/k_credit_card_input_formatters.dart';
 import 'package:ecommerce_shopping_project/utilities/utilities_library_imports.dart';
 
-class PaymentScreenShipping extends ConsumerWidget {
-  const PaymentScreenShipping({
-    super.key,
-    this.isCreateNewAddressMode = false,
-    this.initialAddress,
-  });
+class PaymentScreenShipping extends ConsumerStatefulWidget {
+  const PaymentScreenShipping({super.key, this.isEditingMode = false});
 
-  final bool? isCreateNewAddressMode;
-  final Address? initialAddress;
+  final bool? isEditingMode;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _PaymentScreenShippingState();
+}
+
+class _PaymentScreenShippingState extends ConsumerState<PaymentScreenShipping> {
+  Address? address;
+
+  @override
+  Widget build(BuildContext context) {
+    address = ref.watch(selectedAddress);
+    var controllers = ref.watch(addressTextControllers);
+
     return Scaffold(
       body: SizedBox(
         height: context.mediaQuery.size.height,
         width: context.mediaQuery.size.width,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.min,
           children: [
             Expanded(
               child: ListView(
                 physics: const ClampingScrollPhysics(),
-                padding: EdgeInsets.symmetric(
-                    horizontal: Constants.kMainPaddingHorizontal.w * 2),
+                padding: EdgeInsets.zero,
                 children: [
-                  if (isCreateNewAddressMode == false)
+                  if (widget.isEditingMode == false)
                     const TitleMain(
                       title: AppStrings.paymentScreenTitleShipping,
                       stepNumber: 1,
-                      paddingHorizontal: 0,
                     ),
-                  if (isCreateNewAddressMode!) SizedBox(height: 50.h),
-                  TextformfieldMain(
-                    controller: ref.watch(textControllerAddressFullName(
-                        initialAddress?.fullName)),
-                    hintText: AppStrings.paymentScreenShippingTextField0,
-                  ),
-                  SizedBox(height: 50.h),
-                  TextformfieldMain(
-                    controller: ref.watch(textControllerAddressTitle(
-                        initialAddress?.addressTitle)),
-                    hintText: AppStrings.paymentScreenShippingTextField1,
-                  ),
-                  SizedBox(height: 50.h),
-                  TextformfieldMain(
-                    controller: ref.watch(
-                        textControllerAddress(initialAddress?.addressText)),
-                    hintText: AppStrings.paymentScreenShippingTextField2,
-                    textInputType: TextInputType.multiline,
-                  ),
-                  SizedBox(height: 50.h),
-                  Row(
-                    children: [
-                      Flexible(
-                        child: TextformfieldMain(
-                          controller: ref.watch(
-                              textControllerAddressCity(initialAddress?.city)),
-                          hintText: AppStrings.paymentScreenShippingTextField3,
-                        ),
-                      ),
-                      SizedBox(width: 30.w),
-                      Flexible(
-                        child: TextformfieldMain(
-                          controller: ref.watch(textControllerAddressZipCode(
-                              initialAddress?.zipCode)),
-                          hintText: AppStrings.paymentScreenShippingTextField4,
-                          textInputType: TextInputType.number,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 50.h),
-                  TextformfieldMain(
-                    controller: ref.watch(
-                        textControllerAddressCountry(initialAddress?.country)),
-                    hintText: AppStrings.paymentScreenShippingTextField5,
-                  ),
-                  SizedBox(height: 100.h),
-                  if (isCreateNewAddressMode == false)
-                    SwitchCheckboxMain(
-                      onChanged: () {
-                        ref.read(addressCheckBox.notifier).state =
-                            !ref.read(addressCheckBox.notifier).state;
-                      },
-                      isChecked: ref.watch(addressCheckBox),
-                      text: TextCustom(
-                        text: AppStrings.paymentScreenShippingCheckBox,
-                        textStyle: context.textTheme.labelSmall!,
-                        color: context.colorPalette.text,
-                      ),
+
+                  ///// ADDRESSES TEXTFORMFIELDS
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: Constants.kMainPaddingHorizontal.w * 2,
                     ),
-                  SizedBox(height: 100.h),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextformfieldMain(
+                          hintText: AppStrings.hintAddressFullName,
+                          controller: controllers.fullName,
+                          onChanged: (value) {
+                            controllers.fullName.text = value;
+                            setState(() {});
+                          },
+                          textInputAction: TextInputAction.done,
+                          inputFormatters: [
+                            CreditCardHolderFormatter(),
+                            FilteringTextInputFormatter.allow(
+                                RegExp("[a-zA-Z ]")),
+                          ],
+                        ),
+                        SizedBox(height: 50.h),
+                        TextformfieldMain(
+                          hintText: AppStrings.hintAddressTitle,
+                          controller: controllers.addressTitle,
+                          onChanged: (value) {
+                            controllers.addressTitle.text = value;
+                            setState(() {});
+                          },
+                          inputFormatters: [
+                            LengthLimitingTextInputFormatter(40),
+                          ],
+                        ),
+                        SizedBox(height: 50.h),
+                        TextformfieldMain(
+                          hintText: AppStrings.hintAddressText,
+                          controller: controllers.addressText,
+                          onChanged: (value) {
+                            controllers.addressText.text = value;
+                            setState(() {});
+                          },
+                        ),
+                        SizedBox(height: 50.h),
+                        Row(
+                          children: [
+                            Flexible(
+                              child: TextformfieldMain(
+                                hintText: AppStrings.hintAddressCity,
+                                controller: controllers.city,
+                                onChanged: (value) {
+                                  controllers.city.text = value;
+                                  setState(() {});
+                                },
+                              ),
+                            ),
+                            SizedBox(width: 30.w),
+                            Flexible(
+                              child: TextformfieldMain(
+                                hintText: AppStrings.hintAddressZipCode,
+                                controller: controllers.zipCode,
+                                onChanged: (value) {
+                                  controllers.zipCode.text = value;
+                                  setState(() {});
+                                },
+                                textInputType: TextInputType.number,
+                                inputFormatters: [
+                                  FilteringTextInputFormatter.digitsOnly,
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 50.h),
+                        TextformfieldMain(
+                          hintText: AppStrings.hintAddressCountry,
+                          controller: controllers.country,
+                          onChanged: (value) {
+                            controllers.country.text = value;
+                            setState(() {});
+                          },
+                        ),
+
+                        // ///// SAVED ADDRESSES SLIDER
+                        if (ref.watch(addressesRawList).length > 1 &&
+                            widget.isEditingMode == false)
+                          const HorizontalListviewAddressChips(),
+
+                        SizedBox(height: 80.h),
+                        if (widget.isEditingMode == false && address == null)
+                          SwitchCheckboxMain(
+                            onChanged: () {
+                              ref.read(addressCheckBox.notifier).state =
+                                  !ref.read(addressCheckBox.notifier).state;
+                            },
+                            isChecked: ref.watch(addressCheckBox),
+                            text: TextCustom(
+                              text: AppStrings.saveAddressCheckbox,
+                              textStyle: context.textTheme.labelSmall!,
+                              color: context.colorPalette.text,
+                            ),
+                          ),
+                        SizedBox(height: 100.h),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
-            if (isCreateNewAddressMode == true && initialAddress != null)
+            if (widget.isEditingMode == true && address != null)
               Visibility(
                 visible: context.isKeyboardOpen,
                 child: BottomSheetButtonsProfileUpdateOrDelete(
                   onPressedDelete: () {
-                    if (initialAddress != null) {
-                      ref
-                          .read(addressesProvider.notifier)
-                          .deleteAddress(address: initialAddress!);
-                      context.pop();
-                      ref.read(addressesProvider.notifier).disposeControllers();
-                    }
-                  },
-                  onPressedUpdate: () {
                     ref
                         .read(addressesProvider.notifier)
-                        .updateAddress(initialAddress: initialAddress!);
+                        .deleteAddress(address: address!);
                     context.pop();
-                    ref.read(addressesProvider.notifier).disposeControllers();
+                    ref.read(disposeAddressControllers);
+                  },
+                  onPressedUpdate: () {
+                    ref.read(addressesProvider.notifier).updateAddress(
+                          selectedAddress: address!,
+                          fullName: controllers.fullName.text,
+                          addressTitle: controllers.addressTitle.text,
+                          addressText: controllers.addressText.text,
+                          city: controllers.city.text,
+                          zipCode: controllers.zipCode.text,
+                          country: controllers.country.text,
+                        );
+                    context.pop();
+                    ref.read(disposeAddressControllers);
                   },
                 ),
               ),
-            if (isCreateNewAddressMode == true && initialAddress == null)
+            if (widget.isEditingMode == true && address == null)
               Visibility(
                 visible: context.isKeyboardOpen,
                 child: BottomSheetButtonsProfileUpdateOrDelete(
                   isInCreateMode: true,
                   onPressedCreate: () {
-                    ref.read(addressesProvider.notifier).createAddress();
+                    ref.read(addressesProvider.notifier).createAddress(
+                          fullName: controllers.fullName.text,
+                          addressTitle: controllers.addressTitle.text,
+                          addressText: controllers.addressText.text,
+                          city: controllers.city.text,
+                          zipCode: controllers.zipCode.text,
+                          country: controllers.country.text,
+                        );
                     context.pop();
-                    ref.read(addressesProvider.notifier).disposeControllers();
+                    ref.read(disposeAddressControllers);
                   },
                 ),
               ),
-            if (isCreateNewAddressMode == false)
+            if (widget.isEditingMode == false)
               Visibility(
                 visible: context.isKeyboardOpen,
                 child: BottomSheetButtonsPaymentShipping(
                   onPressed: () {
+                    /// TODO: Enable/Disable Buttons
                     ref
                         .read(paymentScreenNavigationProvider.notifier)
                         .goNextStep(context, ref);

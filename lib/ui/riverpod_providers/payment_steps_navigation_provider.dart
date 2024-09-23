4dbe78cc2eb70e94725601848df3585ffc558b80
Yasin Bilgin <stdxpe@gmail.com>
2024-09-23 16:19:07 +0300

    navigation for payment steps pageview updated

---
+++ lib/ui/riverpod_providers/payment_steps_navigation_provider.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:ecommerce_shopping_project/ui/riverpod_providers/credit_card_providers.dart';
+import 'package:ecommerce_shopping_project/ui/riverpod_providers/shipping_addresses_providers.dart';
+import 'package:ecommerce_shopping_project/services/global_services/navigation_service.dart';
+
+final paymentStepsNavigation =
+    StateNotifierProvider<PaymentStepsNavigationNotifier, int>(
+        (ref) => PaymentStepsNavigationNotifier(ref: ref));
+
+class PaymentStepsNavigationNotifier extends StateNotifier<int> {
+  PaymentStepsNavigationNotifier({required this.ref}) : super(0);
+
+  final StateNotifierProviderRef ref;
+
+  late StatefulNavigationShell _navigationShell;
+
+  setNavigationShell({required StatefulNavigationShell navigationShell}) {
+    _navigationShell = navigationShell;
+  }
+
+  goPreviousPage(BuildContext context) {
+    int currentIndex = _navigationShell.currentIndex;
+
+    (currentIndex > 0)
+        ? _navigationShell.goBranch(currentIndex - 1)
+        : context.pop();
+  }
+
+  goSpecificIndex({required int targetIndex}) {
+    int currentIndex = _navigationShell.currentIndex;
+
+    ref.invalidate(addressProviderForSummary);
+    ref.invalidate(creditCardProviderForSummary);
+
+    var address = ref.read(addressProviderForSummary);
+    var creditCard = ref.read(creditCardProviderForSummary);
+
+    if (targetIndex == 0 && currentIndex != 0) {
+      _navigationShell.goBranch(0);
+    } else if (targetIndex == 1 && currentIndex != 1) {
+      if (address != null) _navigationShell.goBranch(1);
+    } else if (targetIndex == 2 && currentIndex != 2) {
+      if (address != null && creditCard != null) _navigationShell.goBranch(2);
+    } else if (targetIndex == 3 && currentIndex != 3) {
+      if (address != null && creditCard != null) _navigationShell.goBranch(3);
+    } else if (targetIndex == 4 && currentIndex != 4) {
+      if (address != null && creditCard != null) {
+        ref.watch(goRouterProvider).push(Routes.dialogPaymentResult);
+      }
+    }
+  }
+}

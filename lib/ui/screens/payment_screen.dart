d21f1b0224204adb0ae7d5acbc66601d72b16fbf
Yasin Bilgin <stdxpe@gmail.com>
2024-09-23 16:20:59 +0300

    updated according to the riverpod provider changes

--- lib/ui/screens/payment_screen.dart
+++ lib/ui/screens/payment_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-import 'package:ecommerce_shopping_project/ui/riverpod_providers/payment_screen_steps_navigation_provider.dart';
+import 'package:ecommerce_shopping_project/ui/riverpod_providers/payment_steps_navigation_provider.dart';
 import 'package:ecommerce_shopping_project/ui/widgets/app_bars/app_bar_main.dart';
 import 'package:ecommerce_shopping_project/ui/widgets/bottom_navigation_bar/custom_nav_bar_branch_animation.dart';
 import 'package:ecommerce_shopping_project/ui/widgets/switches/switch_payment_status_bar_stepper.dart';
@@ -11,9 +11,7 @@ import 'package:ecommerce_shopping_project/utilities/utilities_library_imports.d
 class PaymentScreen extends ConsumerWidget {
   const PaymentScreen(
       {required this.navigationShell, Key? key, required this.children})
-      : super(
-            key: key ??
-                const ValueKey<String>('ScaffoldWithNavBarPaymentScreen'));
+      : super(key: key ?? const ValueKey('ScaffoldWithNavBarPaymentScreen'));
 
   final StatefulNavigationShell navigationShell;
 
@@ -22,7 +20,7 @@ class PaymentScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     ref
-        .read(paymentScreenNavigationProvider.notifier)
+        .read(paymentStepsNavigation.notifier)
         .setNavigationShell(navigationShell: navigationShell);
     return Scaffold(
       resizeToAvoidBottomInset: false,
@@ -31,22 +29,9 @@ class PaymentScreen extends ConsumerWidget {
         useSearchButton: false,
         automaticallyImplyLeading: true,
         onPressedBackButtonAlternate: () {
-          ref
-              .watch(paymentScreenNavigationProvider.notifier)
-              .goPreviousPage(context);
+          ref.watch(paymentStepsNavigation.notifier).goPreviousPage(context);
         },
       ),
-      // appBar: AppBar(
-      //   toolbarHeight: 175.h,
-      //   title: SwitchPaymentStatusBarStepper(
-      //     activeStep: navigationShell.currentIndex,
-      //     onPressed: (index) {
-      //       ref
-      //           .watch(paymentScreenNavigationProvider.notifier)
-      //           .goSpecificIndex(targetIndex: index);
-      //     },
-      //   ),
-      // ),
       body: SizedBox(
         height: context.mediaQuery.size.height,
         width: context.mediaQuery.size.width,
@@ -54,11 +39,9 @@ class PaymentScreen extends ConsumerWidget {
           children: [
             SwitchPaymentStatusBarStepper(
               activeStep: navigationShell.currentIndex,
-              onPressed: (index) {
-                ref
-                    .watch(paymentScreenNavigationProvider.notifier)
-                    .goSpecificIndex(targetIndex: index);
-              },
+              onPressed: (index) => ref
+                  .watch(paymentStepsNavigation.notifier)
+                  .goSpecificIndex(targetIndex: index),
             ),
             Expanded(
               child: CustomNavBarBranchAnimation(

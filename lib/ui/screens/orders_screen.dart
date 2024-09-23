386c11b2ffb91f376d9e28b31b0e3e9166fbc9b7
Yasin Bilgin <stdxpe@gmail.com>
2024-09-23 20:14:35 +0300

    ui updated

--- lib/ui/screens/orders_screen.dart
+++ lib/ui/screens/orders_screen.dart
@@ -1,35 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'package:ecommerce_shopping_project/ui/widgets/app_bars/app_bar_main.dart';
-import 'package:ecommerce_shopping_project/ui/widgets/listviews_and_gridviews/vertical_listview_order_card_horizontal.dart';
+import 'package:ecommerce_shopping_project/ui/riverpod_providers/order_providers.dart';
+import 'package:ecommerce_shopping_project/ui/widgets/listviews_and_gridviews/listview_order_card_horizontal.dart';
 import 'package:ecommerce_shopping_project/ui/widgets/titles/title_main.dart';
 import 'package:ecommerce_shopping_project/utilities/utilities_library_imports.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends ConsumerWidget {
   const OrdersScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: const AppBarMain(
-        automaticallyImplyLeading: true,
-        useSearchButton: false,
-      ),
       body: SafeArea(
         child: ListView(
           padding: EdgeInsets.zero,
           physics: const ClampingScrollPhysics(),
-          children: const [
+          children: [
             TitleMain(
               title: AppStrings.ordersScreenTitle,
-              itemCount: 2,
+              itemCount: ref.watch(ordersProvider).value?.length ?? 0,
+              enableTitleAsBackButton: true,
+              paddingTop: Constants.kMainTitlePaddingTopWithoutAppBar.h,
+              paddingHorizontal: Constants.kMainPaddingHorizontal.w,
             ),
-            VerticalListviewOrderCardHorizontal(
-              ordersList: [],
-              cardHeight: 200,
-              paddingMain: Constants.kMainPaddingHorizontal,
-              paddingBetweenElements: Constants.kMainSpacingBTWCardsVertical,
-            ),
+            ListviewOrderCardHorizontal(provider: ordersProvider),
           ],
         ),
       ),

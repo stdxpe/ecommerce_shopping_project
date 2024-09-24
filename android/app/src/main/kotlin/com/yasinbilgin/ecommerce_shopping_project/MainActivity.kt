b60428dc01640f92e910e816187a6c9b0b3b0587
Yasin Bilgin <stdxpe@gmail.com>
2024-09-24 16:58:12 +0300

    stripe integration updates on android and ios configs

--- android/app/src/main/kotlin/com/yasinbilgin/ecommerce_shopping_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/yasinbilgin/ecommerce_shopping_project/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.yasinbilgin.ecommerce_shopping_project
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity()

d24427961c961513cffe633fc25ef74c4a9ea17c
Yasin Bilgin <stdxpe@gmail.com>
2024-09-26 13:36:35 +0300

    theme selection saved to local device with hive

--- lib/ui/riverpod_providers/theme_mode_provider.dart
+++ lib/ui/riverpod_providers/theme_mode_provider.dart
@@ -1,25 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final isDarkModeOnProvider = StateProvider<bool>((ref) {
-  return ref.watch(themeModeProvider) == ThemeMode.dark ? true : false;
-});
+import 'package:ecommerce_shopping_project/services/abstract_classes/i_local_db_service.dart';
+import 'package:ecommerce_shopping_project/services/global_services/dependency_injection_service.dart';
+import 'package:ecommerce_shopping_project/utilities/utilities_library_imports.dart';
 
-final themeModeProvider =
-    StateNotifierProvider<ThemeNotifier, ThemeMode>((ref) {
-  return ThemeNotifier();
-});
+final themeMode =
+    StateNotifierProvider<ThemeNotifier, ThemeMode>((ref) => ThemeNotifier());
 
 class ThemeNotifier extends StateNotifier<ThemeMode> {
-  ThemeNotifier() : super(ThemeMode.light);
+  ThemeNotifier() : super(ThemeMode.light) {
+    getInitialThemeModeFromLocalDB();
+  }
+
+  final _localDB = locator<ILocalDBService>();
+
+  getInitialThemeModeFromLocalDB() {
+    String? themeMode = _localDB.getData(key: LocalDB.themeMode);
+
+    if (themeMode == LocalDB.themeModeLight) {
+      state = ThemeMode.light;
+    } else if (themeMode == LocalDB.themeModeDark) {
+      state = ThemeMode.dark;
+    } else {
+      state = ThemeMode.dark;
+    }
+  }
 
   toggleTheme() {
     if (state == ThemeMode.light) {
+      _localDB.updateData(key: LocalDB.themeMode, value: LocalDB.themeModeDark);
+      debugPrint(_localDB.getAllData().toString());
       state = ThemeMode.dark;
     } else if (state == ThemeMode.dark) {
+      _localDB.updateData(
+          key: LocalDB.themeMode, value: LocalDB.themeModeLight);
+      debugPrint(_localDB.getAllData().toString());
       state = ThemeMode.light;
     } else {
       state = ThemeMode.light;
     }
   }
 }
+
+final isDarkModeOn = StateProvider<bool>(
+    (ref) => ref.watch(themeMode) == ThemeMode.dark ? true : false);

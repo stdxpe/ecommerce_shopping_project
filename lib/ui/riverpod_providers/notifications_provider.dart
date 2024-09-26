a089e0f3a1da685271788bbf48c8ef2d88e9b69a
Yasin Bilgin <stdxpe@gmail.com>
2024-09-26 13:36:49 +0300

    notification selection saved to local device with hive

---
+++ lib/ui/riverpod_providers/notifications_provider.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:ecommerce_shopping_project/services/abstract_classes/i_local_db_service.dart';
+import 'package:ecommerce_shopping_project/services/global_services/dependency_injection_service.dart';
+import 'package:ecommerce_shopping_project/utilities/utilities_library_imports.dart';
+
+final notifications = StateNotifierProvider<NotificationsNotifier, bool>(
+    (ref) => NotificationsNotifier());
+
+class NotificationsNotifier extends StateNotifier<bool> {
+  NotificationsNotifier() : super(true) {
+    getInitialNotificationSelectionFromLocalDB();
+  }
+
+  final _localDB = locator<ILocalDBService>();
+
+  getInitialNotificationSelectionFromLocalDB() {
+    String? notifications = _localDB.getData(key: LocalDB.notificationMode);
+
+    if (notifications == LocalDB.notificationsON) {
+      state = true;
+    } else if (notifications == LocalDB.notificationsOFF) {
+      state = false;
+    } else {
+      state = true;
+    }
+  }
+
+  toggleSwitch() {
+    if (state == true) {
+      _localDB.updateData(
+          key: LocalDB.notificationMode, value: LocalDB.notificationsOFF);
+      debugPrint(_localDB.getAllData().toString());
+      state = false;
+    } else if (state == false) {
+      _localDB.updateData(
+          key: LocalDB.notificationMode, value: LocalDB.notificationsON);
+      debugPrint(_localDB.getAllData().toString());
+      state = true;
+    } else {
+      state = true;
+    }
+  }
+}

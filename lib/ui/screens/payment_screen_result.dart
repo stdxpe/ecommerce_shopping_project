bffb4c4d961dd5a0632358f0eefb6b9c2cc8fa46
Yasin Bilgin <stdxpe@gmail.com>
2024-09-27 17:20:39 +0300

    payment result screen recreated with lottie animations

--- lib/ui/screens/payment_screen_result.dart
+++ lib/ui/screens/payment_screen_result.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:lottie/lottie.dart';
 
+import 'package:ecommerce_shopping_project/services/global_services/navigation_service.dart';
 import 'package:ecommerce_shopping_project/ui/riverpod_providers/order_providers.dart';
-import 'package:ecommerce_shopping_project/ui/riverpod_providers/payment_steps_navigation_provider.dart';
-import 'package:ecommerce_shopping_project/ui/riverpod_providers/shopping_cart_providers.dart';
 import 'package:ecommerce_shopping_project/ui/widgets/bottom_sheets/bottom_sheet_buttons_payment_result.dart';
-import 'package:ecommerce_shopping_project/ui/widgets/titles/title_main.dart';
+import 'package:ecommerce_shopping_project/ui/widgets/text_custom.dart';
 import 'package:ecommerce_shopping_project/utilities/utilities_library_imports.dart';
 
 class PaymentScreenResult extends ConsumerWidget {
@@ -13,46 +16,83 @@ class PaymentScreenResult extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final bool result = ref.read(paymentStatus);
+
     return Scaffold(
-      body: SafeArea(
-        bottom: false,
-        child: SizedBox(
-          height: context.mediaQuery.size.height,
-          width: context.mediaQuery.size.width,
+      body: Container(
+        height: context.mediaQuery.size.height,
+        width: context.mediaQuery.size.width,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            gradient: LinearGradient(
+                begin: Alignment.bottomRight,
+                end: Alignment.topCenter,
+                colors: [
+                  Colors.transparent,
+                  result
+                      ? Colors.green.withOpacity(.3)
+                      : Colors.redAccent.withOpacity(.9)
+                  // Colors.blueGrey.withOpacity(.4),
+                ])),
+        child: SafeArea(
+          bottom: false,
           child: Column(
-            mainAxisSize: MainAxisSize.min,
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Expanded(
-                child: ListView(
-                  padding: EdgeInsets.zero,
-                  physics: const ClampingScrollPhysics(),
-                  children: const [
-                    TitleMain(
-                      title: AppStrings.paymentScreenTitleResult,
-                      stepNumber: 4,
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Lottie.asset(
+                      result
+                          ? AppImages.lottieSuccessCheck
+                          : AppImages.lottieError,
+                      repeat: false,
+                      fit: BoxFit.contain,
+                      height: context.mediaQuery.size.width * 0.55,
+                      width: context.mediaQuery.size.width * 0.55,
                     ),
+                    SizedBox(
+                        height: Constants.kDialogPopupSpacingBTWTextVertical.h),
+                    TextCustom(
+                      text: result
+                          ? AppStrings.paymentScreenTitleSuccessResultMessage
+                          : AppStrings.paymentScreenTitleFailedResultMessage,
+                      textStyle: context.textTheme.displaySmall!,
+                      color: context.colorPalette.text,
+                      fontSizeCustom: 100,
+                      fontWeightCustom: FontWeight.w700,
+                      fontHeightCustom: 1.0,
+                      fontLetterSpacingCustom: -0.5,
+                      textAlignCustom: TextAlign.center,
+                    ).animate(delay: 1200.ms).fadeIn(duration: 600.ms),
+                    SizedBox(
+                        height: Constants.kDialogPopupSpacingBTWTextVertical.h *
+                            0.75),
+                    TextCustom(
+                      text: result
+                          ? AppStrings.paymentScreenSubtitleSuccessResultMessage
+                          : AppStrings.paymentScreenSubtitleFailedResultMessage,
+                      textStyle: context.textTheme.displaySmall!,
+                      color: context.colorPalette.text.withOpacity(0.8),
+                      maxLines: 3,
+                      fontSizeCustom: 33,
+                      fontWeightCustom: FontWeight.w500,
+                      fontHeightCustom: 1.4,
+                      fontLetterSpacingCustom: 0,
+                      textAlignCustom: TextAlign.center,
+                      isHeightConstraintRelated: false,
+                    ).animate(delay: 1600.ms).fadeIn(duration: 500.ms),
                   ],
                 ),
               ),
               Align(
                 alignment: Alignment.bottomCenter,
                 child: BottomSheetButtonsPaymentResult(
-                  onPressed: () {
-                    /// TODO: Stripe Result Here
-
-                    /// Order Created on DB
-                    ref.read(ordersProvider.notifier).createOrder();
-
-                    /// Shopping Cart Resetted
-                    ref
-                        .read(shoppingCartProvider.notifier)
-                        .resetShoppingCartAfterCheckout();
-
-                    /// Directing to the Dialog Popup Screen
-                    ref
-                        .read(paymentStepsNavigation.notifier)
-                        .goSpecificIndex(targetIndex: 4);
-                  },
+                  useTransparentBg: true,
+                  onPressed: () => context.go(Routes.home),
                 ),
               ),
             ],
